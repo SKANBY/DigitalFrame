e2603eb797596dddf1ae268bc469d04eec7b48df
김나영 <KNY@gimnayeongui-MacBook-Pro.local>
2017-12-14 16:20:08 +0900

    토글 버튼 기능 구현

--- DigitalFrame/ViewController.swift
+++ DigitalFrame/ViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var imgView:UIImageView!
+    @IBOutlet var toggleButton:UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let cuteImages = [UIImage(named:"1.jpg")!,
+        let cuteImages = [UIImage(named:"1.jpg")!,  //이미지 초기화
                           UIImage(named:"2.jpg")!,
                           UIImage(named:"3.jpg")!,
                           UIImage(named:"4.jpg")!,
@@ -30,8 +31,20 @@ class ViewController: UIViewController {
                           UIImage(named:"15.jpg")!]
         
         imgView.animationImages = cuteImages
+        imgView.animationDuration = 15.0    //15초
+        
     }
 
+    @IBAction func toggleAction(_ sender:AnyObject){
+        if imgView.isAnimating {    //돌고 있는 중이라면 true값이 리턴되므로
+            imgView.stopAnimating() //버튼 눌렀을때는 중지되야 한다
+            toggleButton.setTitle("Start", for: UIControlState.normal)
+        }else {
+            imgView.startAnimating()
+            toggleButton.setTitle("Stop", for: UIControlState.normal)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
