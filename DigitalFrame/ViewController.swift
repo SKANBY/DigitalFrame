dc18d7b1b0cada2693e182cca235deeda59c8b21
김나영 <KNY@gimnayeongui-MacBook-Pro.local>
2017-12-14 16:52:11 +0900

    slider 기능 구현, 버튼 기능 추가

--- DigitalFrame/ViewController.swift
+++ DigitalFrame/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var imgView:UIImageView!
     @IBOutlet var toggleButton:UIButton!
+    @IBOutlet var speedSlider:UISlider!
+    @IBOutlet var speedLabel:UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +33,7 @@ class ViewController: UIViewController {
                           UIImage(named:"15.jpg")!]
         
         imgView.animationImages = cuteImages
-        imgView.animationDuration = 15.0    //15초
+        imgView.animationDuration = 15.0    //속도 조절, 15초
         
     }
 
@@ -43,6 +45,17 @@ class ViewController: UIViewController {
             imgView.startAnimating()
             toggleButton.setTitle("Stop", for: UIControlState.normal)
         }
+    }
+    
+    @IBAction func changeSpeedAction(_ sender:AnyObject){
+        //slider값이 변경될 때마다 이미지가 change되는 속도가 변한다
+        //-> 굳이 Start버튼을 누르지 않더라도 Slider값이 변경되면 이미지가 animating 된다
+        //(Stop버튼이 눌리기 전까진 이미지가 멈추진 않음)
+        imgView.animationDuration = Double(speedSlider.value)   //slider의 값을 가져온다
+        imgView.startAnimating()
+        toggleButton.setTitle("Stop", for: UIControlState.normal)
+        
+        speedLabel.text = String(format:"%.2f", speedSlider.value)
     }
     
     override func didReceiveMemoryWarning() {
